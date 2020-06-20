21f59dc046d8cd9c0d89bf49ea7a028495a5e9ad
Michael Spector <spektom@gmail.com>
2020-06-20 21:28:30 +0300

    Remove unneeded script

--- manager/feeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO feed(uri, name, category) VALUES
-  ('https://feeds.a.dj.com/rss/RSSWSJD.xml', 'WSJ', 'Technology: What''s News'),
-  ('https://www.theverge.com/tech/rss/index.xml', 'The Verge', 'Tech Posts'),
-  ('https://www.zdnet.com/news/rss.xml', 'ZDNet', 'Latest News'),
-  ('https://venturebeat.com/feed/', 'VentureBeat', 'Latest News'),
-  ('https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml', 'NYT', 'Technology');
