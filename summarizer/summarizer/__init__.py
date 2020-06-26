ac79bf5cd4a777fc2133e4be1566dc24c85b3dee
Michael Spector <spektom@gmail.com>
2020-06-26 10:44:44 +0300

    Move to spaCy

--- summarizer/summarizer/__init__.py
+++ summarizer/summarizer/__init__.py
@@ -1,46 +1,18 @@
 import joblib
 import logging
-import nltk
 import re
+import spacy
 
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
-from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-for p in ['stopwords', 'punkt', 'wordnet', 'averaged_perceptron_tagger']:
-    nltk.download(p, download_dir='.data', quiet=True)
 
-nltk.data.path.append('.data')
-
-stopwords_ = set(stopwords.words('english'))
-stemmer_ = SnowballStemmer('english')
-lemmatizer_ = WordNetLemmatizer()
-nouns_ = set(['NN', 'NNS', 'NNP', 'NNPS'])
-pronouns_ = set(['PRP', 'PRP$'])
-
-
-def retain_nouns(words):
-    return [word for word, pos in nltk.pos_tag(words) if pos in nouns_]
-
-
-def drop_stopwords(words):
-    return [word for word in words if word not in stopwords_]
-
-
-def drop_nonwords(words):
-    return [word for word in words if re.search(r'[A-Za-z]{3,}', word)]
-
-
-def stem_or_lemmatize_token(token):
-    s = stemmer_.stem(token)
-    if s == token:
-        s = lemmatizer_.lemmatize(token)
-    return s
-
-
-def stem_or_lemmatize(tokens):
-    return [stem_or_lemmatize_token(t) for t in tokens]
+def nlp(doc):
+    global nlp_
+    if 'nlp_' not in globals():
+        model = 'en_core_web_md'
+        logging.info(f'Loading NLP model: {model}')
+        nlp_ = spacy.load(model)
+    return nlp_(doc)
 
 
 def drop_non_sentences(text):
@@ -53,24 +25,19 @@ def drop_non_sentences(text):
     return text
 
 
-def drop_pronouns_sentences(sentences):
-    return [
-        sentence for sentence in sentences if nltk.pos_tag(
-            [drop_nonwords(nltk.word_tokenize(sentence))[0]])[0][1] not in pronouns_
+def clean_for_training(doc):
+    if not isinstance(doc, spacy.tokens.doc.Doc):
+        doc = nlp(doc)
+    lemmas = [
+        token.lemma_ for token in doc
+        if not token.is_stop and re.search(r'[A-Za-z]{3,}', token.lemma_)
     ]
-
-
-def clean_for_training(doc=None, sentences=None):
-    sentences = sentences or nltk.sent_tokenize(doc)
-    return ' '.join([
-        word for sentence in sentences for word in stem_or_lemmatize(
-            drop_stopwords(drop_nonwords(nltk.word_tokenize(sentence))))
-    ])
+    return ' '.join(lemmas)
 
 
 def train_and_save(docs, model_file='dtm.model'):
     logging.info('Started procesing %d documents', len(docs))
-    docs = [clean_for_training(doc=drop_non_sentences(doc)) for doc in docs]
+    docs = [clean_for_training(drop_non_sentences(doc)) for doc in docs]
 
     tfidf = TfidfVectorizer()
     logging.info('Started building document-term matrix')
@@ -83,29 +50,24 @@ def train_and_save(docs, model_file='dtm.model'):
 
 
 def summarize(tfidf, feature_indices, doc, title, top_n=5):
-    doc = drop_non_sentences(doc)
+    doc = nlp(drop_non_sentences(doc))
 
     logging.info('Building document terms frequency')
 
-    # Save original sentences for constructing a summary later
-    sentences = drop_pronouns_sentences(nltk.sent_tokenize(doc))
-
-    doc_freq = tfidf.transform([clean_for_training(sentences=sentences)
-                                ]).todense().tolist()[0]
+    doc_freq = tfidf.transform([clean_for_training(doc)]).todense().tolist()[0]
 
     logging.info('Ranking sentences')
 
-    tokenized_sentences = [
-        nltk.word_tokenize(sentence.lower()) for sentence in sentences
+    # Drop sentences containing pronouns (he, she, etc.)
+    sentences = [
+        sent for sent in doc.sents if len([t for t in sent if t.pos_ == 'PRON']) == 0
     ]
 
     # Word frequencies in sentences (only nouns are taken into account)
-    sentences_nouns = [
-        stem_or_lemmatize(retain_nouns(sentence)) for sentence in tokenized_sentences
-    ]
     sentences_freqs = [[
-        doc_freq[feature_indices[word]] for word in words if word in feature_indices
-    ] for words in sentences_nouns]
+        doc_freq[feature_indices[t.lemma_]] for t in sent
+        if t.pos_ == 'NOUN' and t.lemma_ in feature_indices
+    ] for sent in sentences]
 
     # Sentences frequences
     doc_freq_sum = sum(doc_freq)
@@ -117,16 +79,9 @@ def summarize(tfidf, feature_indices, doc, title, top_n=5):
     ranked_sentences_indices = enumerate(sentences_freqs)
     ranked_sentences_num = len(sentences_freqs)
 
-    # Add similarity to title to sentence scores
-    title_tokens = set(
-        [t.lower() for t in drop_stopwords(drop_nonwords(nltk.word_tokenize(title)))])
-    sentences_tokens = [
-        drop_stopwords(drop_nonwords(words)) for words in tokenized_sentences
-    ]
-    similarity_scores = [
-        len([t for t in tokens if t in title_tokens]) * 0.1 / len(title_tokens)
-        for tokens in sentences_tokens
-    ]
+    # Increment scores of sentences similar to title
+    title_doc = nlp(title)
+    similarity_scores = [sent.similarity(title_doc) * 0.1 for sent in sentences]
     ranked_sentences_indices = [(index, rank + similarity_scores[index])
                                 for index, rank in ranked_sentences_indices]
 
@@ -142,8 +97,8 @@ def summarize(tfidf, feature_indices, doc, title, top_n=5):
     result = [title]
     for index, _ in ranked_sentences_indices:
         sentence = sentences[index]
-        if sentence not in result:
-            result.append(sentence)
+        if sentence.text not in result:
+            result.append(sentence.text)
         if len(result) >= top_n:
             break
     return result
@@ -157,4 +112,4 @@ def create_summarizer(model_file='dtm.model'):
     feature_indices = {n: i for i, n in enumerate(feature_names)}
 
     logging.info(f'Initializing summarizer')
-    return lambda doc, title: summarize(tfidf, feature_indices, doc, title)
+    return lambda text, title: summarize(tfidf, feature_indices, text, title)
