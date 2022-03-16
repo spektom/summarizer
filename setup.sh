2c80e1b6910365b33ca403b2456e9eb069c102b7
Michael Spector <spektom@gmail.com>
2022-03-16 20:27:22 +0000

    Added needed dependency

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -eu
 
 sudo apt-get install -y \
-	build-essential \
-	jq \
-	libffi-dev \
-	libssl-dev \
-	python3-dev \
+  build-essential \
+  jq \
+  libffi-dev \
+  libssl-dev \
+  python3-dev \
   gfortran \
   liblapack-dev \
   libopenblas-dev \
   libxml2-dev \
   libxslt-dev \
-	zip
+  zip \
+  sqlite3
 
 for f in $(ls */setup.sh); do
   (cd $(dirname $f); ./setup.sh)
