8c9caf729d3a011959cb82019a912d6d8777d8b0
Michael Spector <spektom@gmail.com>
2022-03-26 18:57:13 +0000

    Fixed filter

--- manager/manager/api.py
+++ manager/manager/api.py
@@ -11,70 +11,77 @@ from sqlalchemy.sql.expression import func
 from .app import app, db
 from .model import Feed, Article
 
-feedparser.USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'
-summarizer_url = 'http://localhost:6000'
-bot_url = 'http://localhost:7000'
+feedparser.USER_AGENT = (
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
+)
+summarizer_url = "http://localhost:6000"
+bot_url = "http://localhost:7000"
 
 
 def is_blacklisted(uri):
-    for pattern in [r'.*/www.youtube.com/.*']:
+    for pattern in [r".*/www.youtube.com/.*"]:
         if re.match(pattern, uri):
             return True
     return False
 
 
 def is_news_title(title):
-    r = requests.post(f'{summarizer_url}/isnewstitle', json={'title': title})
+    r = requests.post(f"{summarizer_url}/isnewstitle", json={"title": title})
     r.raise_for_status()
-    return r.json()['value']
+    return r.json()["value"]
 
 
 def publish_article(article):
-    r = requests.post(f'{summarizer_url}/summarize',
-                      json={
-                          'id': article.id,
-                          'title': article.title,
-                          'html': article.html,
-                          'uri': article.uri,
-                          'create_time': article.create_time.isoformat()
-                      })
+    r = requests.post(
+        f"{summarizer_url}/summarize",
+        json={
+            "id": article.id,
+            "title": article.title,
+            "html": article.html,
+            "uri": article.uri,
+            "create_time": article.create_time.isoformat(),
+        },
+    )
     r.raise_for_status()
     res = r.json()
 
-    source = 'Anonymous'
+    source = "Anonymous"
     if article.source is not None:
         source = article.source
     elif article.feed_id is not None:
         source = Feed.query.get(article.feed_id).name
 
     # Calculate article importance based on number of similar articles during last 12 hours
-    importance = len([score for score in res['similar_articles'] if score > 0.3])
+    importance = len([score for score in res["similar_articles"] if score > 0.3])
 
-    r = requests.post(f'{bot_url}/summary',
-                      json={
-                          'source': source,
-                          'title': article.title,
-                          'uri': article.uri,
-                          'summary': res['summary'],
-                          'importance': importance
-                      })
-    r.raise_for_status()
+    if importance > 0:
+        r = requests.post(
+            f"{bot_url}/summary",
+            json={
+                "source": source,
+                "title": article.title,
+                "uri": article.uri,
+                "summary": res["summary"],
+                "importance": importance,
+            },
+        )
+        r.raise_for_status()
 
 
-@app.route('/feeds/refresh', methods=['GET'])
+@app.route("/feeds/refresh", methods=["GET"])
 def feeds_refresh():
     for feed in Feed.query.all():
         try:
-            app.logger.info(f'Refreshing RSS feed: {feed.uri}')
+            app.logger.info(f"Refreshing RSS feed: {feed.uri}")
             last_publish_time = feed.last_publish_time
-            parsed_feed = feedparser.parse(feed.uri,
-                                           etag=feed.etag,
-                                           modified=feed.modified)
+            parsed_feed = feedparser.parse(
+                feed.uri, etag=feed.etag, modified=feed.modified
+            )
 
             for entry in parsed_feed.entries:
-                if 'published_parsed' in entry:
+                if "published_parsed" in entry:
                     entry.update_time = entry.published_parsed
-                elif 'updated_parsed' in entry:
+                elif "updated_parsed" in entry:
                     entry.update_time = entry.updated_parsed
                 else:
                     entry.update_time = localtime()
@@ -82,7 +89,7 @@ def feeds_refresh():
             for entry in sorted(parsed_feed.entries, key=lambda e: e.update_time):
                 publish_time = datetime.fromtimestamp(mktime(entry.update_time))
 
-                if not 'link' in entry:
+                if not "link" in entry:
                     continue
 
                 if last_publish_time is None or last_publish_time < publish_time:
@@ -91,34 +98,40 @@ def feeds_refresh():
                     if feed.is_aggregator:
                         # Find out the original URI to avoid duplications
                         try:
-                            article_uri = requests.head(article_uri,
-                                                        allow_redirects=True).url
+                            article_uri = requests.head(
+                                article_uri, allow_redirects=True
+                            ).url
                         except:
                             continue
 
                     if is_blacklisted(article_uri):
                         continue
 
-                    article_uri = re.sub(r'[?#].*', '', article_uri)
+                    article_uri = re.sub(r"[?#].*", "", article_uri)
 
                     # Determine source in case of aggregator
                     source = None
-                    if feed.is_aggregator and hasattr(entry, 'source'):
+                    if feed.is_aggregator and hasattr(entry, "source"):
                         source = entry.source.title
 
                     # Analyze the title, and see whether it worth adding the article
-                    if hasattr(entry, 'title') and not is_news_title(entry.title):
-                        app.logger.info(f'Skipping non-news article: {article_uri}')
+                    if hasattr(entry, "title") and not is_news_title(entry.title):
+                        app.logger.info(f"Skipping non-news article: {article_uri}")
                         continue
 
                     try:
-                        app.logger.info(f'Adding new article: {article_uri}')
+                        app.logger.info(f"Adding new article: {article_uri}")
                         db.session.add(
-                            Article(feed_id=feed.id,
-                                    uri=article_uri,
-                                    summary=entry.summary if hasattr(entry, 'summary') else None, \
-                                    status='N',
-                                    source=source))
+                            Article(
+                                feed_id=feed.id,
+                                uri=article_uri,
+                                summary=entry.summary
+                                if hasattr(entry, "summary")
+                                else None,
+                                status="N",
+                                source=source,
+                            )
+                        )
                         db.session.commit()
 
                     except exc.IntegrityError:  # Existing URI
@@ -126,7 +139,8 @@ def feeds_refresh():
 
                         # Increment references count
                         existing = Article.query.filter(
-                            Article.uri == article_uri).first()
+                            Article.uri == article_uri
+                        ).first()
                         existing.refs_count += 1
                         db.session.commit()
 
@@ -134,68 +148,72 @@ def feeds_refresh():
 
             # Update feed's properties
             feed.last_publish_time = last_publish_time
-            if hasattr(parsed_feed, 'etag'):
+            if hasattr(parsed_feed, "etag"):
                 feed.etag = parsed_feed.etag
-            if hasattr(parsed_feed, 'modified'):
+            if hasattr(parsed_feed, "modified"):
                 feed.modified = parsed_feed.modified
             db.session.commit()
 
         except:
             db.session.rollback()
-            app.logger.exception(f'Failed to refresh RSS feed: {feed.uri}')
+            app.logger.exception(f"Failed to refresh RSS feed: {feed.uri}")
 
-    return '', 204
+    return "", 204
 
 
-@app.route('/tasks/reschedule', methods=['GET'])
+@app.route("/tasks/reschedule", methods=["GET"])
 def tasks_reschedule():
     hour_ago = datetime.utcnow() - timedelta(hours=1)
-    for article in Article.query.filter(Article.status == 'Q'
-                                        and Article.update_time < hour_ago).all():
+    for article in Article.query.filter(
+        (Article.status == "Q") & (Article.update_time < hour_ago)
+    ):
         article.retries += 1
         if article.retries < 3:
-            article.status = 'N'
+            article.status = "N"
         else:
-            article.status = 'E'
+            article.status = "E"
     db.session.commit()
-    return '', 204
+    return "", 204
 
 
-@app.route('/tasks/next', methods=['GET'])
+@app.route("/tasks/next", methods=["GET"])
 def tasks_next():
-    article = Article.query.filter(Article.status == 'N').order_by(
-        func.random()).first()
+    article = (
+        Article.query.filter(Article.status == "N").order_by(func.random()).first()
+    )
     if article is not None:
-        article.status = 'Q'
+        article.status = "Q"
         db.session.commit()
         return jsonify(id=article.id, uri=article.uri)
-    return '', 204
+    return "", 204
 
 
-@app.route('/article/<id>', methods=['POST'])
+@app.route("/article/<id>", methods=["POST"])
 def save_article(id):
     article = Article.query.get_or_404(id)
-    article.html = request.json['html']
-    article.title = request.json['title']
-    article.status = 'D'
+    article.html = request.json["html"]
+    article.title = request.json["title"]
+    article.status = "D"
     db.session.commit()
 
     publish_article(article)
-    return '', 200
+    return "", 200
 
 
-@app.route('/pagesaver/log', methods=['POST'])
+@app.route("/pagesaver/log", methods=["POST"])
 def pagesaver_log():
-    app.logger.log(getattr(logging, request.json['level']),
-                   f"<PageSaver> [{request.json['ts']}] {request.json['message']}")
-    return '', 200
+    app.logger.log(
+        getattr(logging, request.json["level"]),
+        f"<PageSaver> [{request.json['ts']}] {request.json['message']}",
+    )
+    return "", 200
 
 
-@app.route('/publish/<article_id>', methods=['GET'])
+@app.route("/publish/<article_id>", methods=["GET"])
 def pipeline_run(article_id):
     article = Article.query.get_or_404(article_id)
-    if article.status != 'D' or article.html is None:
-        return f'Article #{article_id} not been fetched yet', 400
+    if article.status != "D" or article.html is None:
+        return f"Article #{article_id} not been fetched yet", 400
 
     publish_article(article)
-    return '', 200
+    return "", 200
