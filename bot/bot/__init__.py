fffb05f256dea3c9df65b85b6903352c53f17eb7
Michael Spector <spektom@gmail.com>
2022-04-08 03:27:50 +0000

    send directly to news channel

--- bot/bot/__init__.py
+++ bot/bot/__init__.py
@@ -4,33 +4,51 @@ import telegram
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
 
-token = os.getenv('TOKEN')
+token = os.getenv("TOKEN")
 bot = telegram.Bot(token=token)
 
 
 def send_to_editors(source, uri, title, summary, importance):
-    summary_text = '\n'.join([f'— {sentence}' for sentence in summary])
+    summary_text = "\n".join([f"— {sentence}" for sentence in summary])
     message = f'<b>{title}</b>\n\n{summary_text}\n\n<a href="{uri}"><i>{source}</i></a>'
 
-    icon = '\U0001F525' * importance
+    icon = "\U0001F525" * importance
     reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton(f'Promote {icon}', callback_data='promote')]])
+        [[InlineKeyboardButton(f"Promote {icon}", callback_data="promote")]]
+    )
 
-    bot.send_message(chat_id=-1001306868978,
-                     parse_mode='html',
-                     disable_web_page_preview=True,
-                     disable_notification=True,
-                     text=message,
-                     reply_markup=reply_markup)
+    bot.send_message(
+        chat_id=-1001306868978,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        disable_notification=True,
+        text=message,
+        reply_markup=reply_markup,
+    )
 
 
 def promote_message(message):
-    bot.send_message(chat_id='@brieftech',
-                     parse_mode='html',
-                     disable_web_page_preview=True,
-                     disable_notification=True,
-                     text=message.text_html)
+    bot.send_message(
+        chat_id="@brieftech",
+        parse_mode="html",
+        disable_web_page_preview=True,
+        disable_notification=True,
+        text=message.text_html,
+    )
 
-    bot.edit_message_reply_markup(message.chat.id,
-                                  message.message_id,
-                                  reply_markup=InlineKeyboardMarkup([[]]))
+    bot.edit_message_reply_markup(
+        message.chat.id, message.message_id, reply_markup=InlineKeyboardMarkup([[]])
+    )
+
+
+def send_directly(source, uri, title, summary):
+    summary_text = "\n".join([f"— {sentence}" for sentence in summary])
+    message = f'<b>{title}</b>\n\n{summary_text}\n\n<a href="{uri}"><i>{source}</i></a>'
+
+    bot.send_message(
+        chat_id="@brieftech",
+        parse_mode="html",
+        disable_web_page_preview=True,
+        disable_notification=True,
+        text=message,
+    )
--- bot/bot/api.py
+++ bot/bot/api.py
@@ -1,16 +1,32 @@
 import logging
 
 from flask import request, jsonify, Flask
-from . import send_to_editors
+from . import send_to_editors, send_directly
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(module)s: %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s %(levelname)s %(module)s: %(message)s"
+)
 
 app = Flask(__name__)
 
+CURATED = False
 
-@app.route('/summary', methods=['POST'])
+
+@app.route("/summary", methods=["POST"])
 def summary():
-    send_to_editors(request.json['source'], request.json['uri'], request.json['title'],
-                    request.json['summary'], request.json.get('importance', 0))
-    return '', 200
+    if CURATED:
+        send_to_editors(
+            request.json["source"],
+            request.json["uri"],
+            request.json["title"],
+            request.json["summary"],
+            request.json.get("importance", 0),
+        )
+    else:
+        send_directly(
+            request.json["source"],
+            request.json["uri"],
+            request.json["title"],
+            request.json["summary"],
+        )
+    return "", 200
